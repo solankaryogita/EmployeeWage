7d4d20bdde11d4f826bc363d598f755b51a7aeda
PoojaSolankar <poojasolankar09gmail.com>
2022-03-02 19:55:12 +0530

    Employee Wage

--- Employee.java
+++ Employee.java
@@ -4,21 +4,31 @@ import java.util.Random;
 
 public class Employee 
 {
-   
+	static final int WAGE_PER_HOURS =20;
+	
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Employee Wages");
-		
-		int randomVal = (int)Math.floor(Math.random()*10) %2;
+              int workingHours;
+	      int Employeewag;
+	      System.out.println("Welcome to Employee Wages");
+	        
+	     int randomVal = (int)Math.floor(Math.random()*10) %2;
 	      
-	    System.out.println("Random value:"+randomVal);
+	     System.out.println("Random value:"+randomVal);
 	   
 	     if(randomVal ==1) 
-	      {
+	     {
+	    	 
 	           System.out.println("Employee is Present");
-	      }
-	     else
+	           workingHours=8;
+	           System.out.println("Working hours :"+workingHours);
+	           Employeewag=workingHours*WAGE_PER_HOURS;
+	           System.out.println("Employeewag is :"+Employeewag);
+	     }
+	     else {
 	           System.out.println("Employee is Absent");
-		
+	           workingHours=0;
+	           System.out.println("Working hours :"+workingHours);
+	          }
     }
 }
