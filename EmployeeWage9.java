1d893f8b1e9d8c3148024d0df3d2c1fd2ae7a3f8
PoojaSolankar <poojasolankar09gmail.com>
2022-03-12 20:29:31 +0530

    Emp Wage with Instance Variable

--- EmployeeWage9.java
+++ EmployeeWage9.java
@@ -1,16 +1,27 @@
 package com.Bridgelabz;
 
 public class EmployeeWage9 {
+	
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NO_OF_WORKING_DAYS= 2;
-	public static final int MAX_HRS_IN_MONTH = 100;
+	private final String company;
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxHrsPerMonth;
 	
-	public int calculateempwageforCompany(String company, int empRate, int numOfDays, int maxHrs)
+	public EmployeeWage9(String company, int empRatePerHour, int numOfWorkingDays, int maxHrsPerMonth)
 	{
+		super();
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHrsPerMonth = maxHrsPerMonth;
+	}
+	public static void main(String[] args) {
+		EmployeeWage9 emp = new EmployeeWage9("Relince", 20, 2, 10);
+		
 		int empHrs = 0, totalEmpHrs = 0, totalWrkingDays = 0,  totalEmpWage=0;
-		while(totalEmpHrs <= maxHrs && totalWrkingDays <numOfDays)
+		while(totalEmpHrs <= emp.maxHrsPerMonth && totalWrkingDays < emp.numOfWorkingDays)
 		{
 			totalWrkingDays++;
 			int empCheck = (int)Math.floor(Math.random() * 10) %3;
@@ -33,19 +44,7 @@ public class EmployeeWage9 {
 			totalEmpHrs =totalEmpHrs + empHrs;
 			System.out.println("Day#: " + totalWrkingDays + "Emp Hr: " + empHrs);
 		}
-		    
-			
-			int salary = empHrs * EMP_RATE_PER_HOUR;
-			System.out.println("salary ----------"+salary);
-			totalEmpWage = totalEmpWage+totalEmpHrs *empRate;
-			System.out.println("totalEmp wage for company: " +company+" is: "+totalEmpWage);
-			return totalEmpWage;
-	}
-	public static void main(String[] args) {
-		EmployeeWage9 emp = new EmployeeWage9();
-		EmployeeWage9 emp1 = new EmployeeWage9();
-		emp.calculateempwageforCompany("Relince", 20, 5, 16);
-		System.out.println("-----------------------------------------");
-		emp1.calculateempwageforCompany("City Central Mall", 10, 2, 7);
+		    totalEmpWage = totalEmpHrs *emp.empRatePerHour;
+			System.out.println("totalEmp wage for Company is: "+totalEmpWage);
 	}
 }
